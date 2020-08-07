f356298b4d3bbf1c5e1c23c7c7bb9a84028bf3c7
fakorede <abiolafakorede@gmail.com>
2020-08-07 19:07:11 +0100

    formatting response using API Resources

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'username' => $this->username,
+            'name' => $this->name,
+            'email' => $this->email,
+            'tagline' => $this->tagline,
+            'about' => $this->about,
+            'location' => $this->location,
+            'available_to_hire' => $this->available_to_hire,
+            'formatted_address' => $this->formatted_address,
+            'dates' => [
+                'created_at' => $this->created_at,
+                'formatted_created_at' => $this->created_at->diffForHumans,
+            ],
+        ];
+    }
+}
