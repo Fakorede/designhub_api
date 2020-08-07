2f807d3310230c3355fa23e963925b9da281c407
fakorede <abiolafakorede@gmail.com>
2020-08-07 22:44:03 +0100

    Customising the responses for password reset

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,6 @@ Route::group(['middleware' => 'guest:api'], function () {
     Route::post('verification/verify/{user}', 'Auth\VerificationController@verify')->name('verification.verify');
     Route::post('verification/resend', 'Auth\VerificationController@resend')->name('verification.resend');
     Route::post('login', 'Auth\LoginController@login')->name('login');
-    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('reset.password.email');
     Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 });
