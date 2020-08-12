0d445462a7387cbaa915e70cd5b5cc407ea98f8b
fakorede <abiolafakorede@gmail.com>
2020-08-12 18:21:45 +0100

    updating design information

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::group(['middleware' => 'auth:api'], function () {
 
     // Upload Designs
     Route::post('designs', 'Designs\UploadController@upload');
+    Route::put('designs/{id}', 'Designs\DesignController@update');
 
 });
 
