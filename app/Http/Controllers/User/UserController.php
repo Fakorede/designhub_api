ffc34effd98c9634845f330819cc1d1665655d25
fakorede <abiolafakorede@gmail.com>
2020-08-14 15:47:50 +0100

    repositories for users and designs

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,14 +4,20 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
-use App\Models\User;
-use Illuminate\Http\Request;
+use App\Repositories\Contracts\UserInterface;
 
 class UserController extends Controller
 {
+    protected $users;
+
+    public function __construct(UserInterface $users)
+    {
+        $this->users = $users;
+    }
+
     public function index()
     {
-        $users = User::all();
+        $users = $this->users->all();
         return UserResource::collection($users);
     }
 }
---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Repositories\Contracts\{UserInterface, DesignInterface};
+use App\Repositories\Eloquent\{UserRepository, DesignRepository};
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->bind(UserInterface::class, UserRepository::class);
+        $this->app->bind(DesignInterface::class, DesignRepository::class);
+    }
+}
